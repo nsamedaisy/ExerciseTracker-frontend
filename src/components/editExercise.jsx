7c49c77b2216543e7f2e132de1937f28d1c514ef
nsamedaisy <nsamedaisy@gmail.com>
2023-08-10 17:09:07 +0100

    frontend exercise tracker commit

---
+++ src/components/editExercise.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function editExercise() {
+  return (
+    <div>editExercise</div>
+  )
+}
---
+++ src/components/exercisesList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function exercisesList() {
+  return (
+    <div>exercisesList</div>
+  )
+}
